84b6b646f9cec661e6988ca89d2bc387367c6a71
FelipeMMoraes <felipe.moraes8@hotmail.com>
2023-03-30 16:22:01 -0300

    fix: Logo Object

--- src/objects/Logo/index.js
+++ src/objects/Logo/index.js
@@ -1,11 +1,10 @@
 import React from 'react'
-import logo from '../../assets/img/logo.png'
+
+import CollabCode from '../../assets/img/logo.png'
 import './styles.css'
 
-const Logo = () => {
-  return (
-    <img src={logo} className='logo' alt='Logo CollabCode'/>
-  )
-}
+const Logo = () => (
+  <img src={CollabCode} alt="Logo da CollabCode" className='logo'/>
+)
 
 export default Logo
